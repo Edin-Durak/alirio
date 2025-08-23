85b3b3bbe2ddf144230ef91fa45dd0b56f4055f9
Edin <edindurak8@gmail.com>
2025-08-23 23:30:19 +0200

    feat/i18n-support; add internationalization configuration with default locale and supported languages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,11 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://edin-durak.github.io",
   // base: "/alirio",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "sv", "bs", "ar", "ku", "sy"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
 });
