8f9cc85255790c1fdb4191093c5b89c1c515c2c9
Edin <edindurak8@gmail.com>
2025-08-23 22:59:54 +0200

    fix header and footer links; remove base path from links for home, services, about, pricing, contact, and privacy to streamline navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://edin-durak.github.io",
-  base: "/alirio",
+  // base: "/alirio",
 });
