9c3791569b3fad342cbff72c5d52fe7a1433e2a8
Edin <edindurak8@gmail.com>
2025-08-31 18:40:24 +0200

    chore(config): comment out base path in astro.config.mjs for future reference

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://edin-durak.github.io",
-  base: "/alirio",
+  // base: "/alirio",
   i18n: {
     defaultLocale: "sv",
     locales: ["en", "sv", "bs", "ar"],
